1b89903257a540f762bf14cce2ca601d64e71cda
Giulia Freulon <giuliaaraujof@hotmail.com>
2024-03-15 17:09:24 -0300

    contador

---
+++ countdown/src/App.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { Outlet } from "react-router-dom";
+
+import { CountdownContext } from "./context/CountdownContext";
+
+import Background from "./assets/background.jpg";
+
+import "./App.css";
+
+function App() {
+  const { event } = useContext(CountdownContext);
+
+  let eventImage = null;
+  if (event) eventImage = event.image;
+
+  return (
+    <div
+      className="App"
+      style={
+        eventImage
+          ? { backgroundImage: `url(${eventImage})` }
+          : { backgroundImage: `url(${Background})` }
+      }
+    >
+      <div className="container">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default App;
